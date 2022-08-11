31a865b6aaae9cfb7c721ad8c4d48390f529a55d
KiberBomj2070 <kiber.bomj2070@gmail.com>
2022-08-11 18:24:21 +0300

    new wrong app

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('direct/inbox/', include('inbox.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
